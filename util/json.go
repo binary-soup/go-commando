dde8a90f8c4b29a2bdd773e9cde9518741c7dcac
Ryan Stamp <apps@binarysoup.dev>
2025-06-15 13:48:09 -0400

    Add save json func to util + add docs to json funcs

--- util/json.go
+++ util/json.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Load the generic type from a JSON file.
 func LoadJSON[T any](name, path string) (*T, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -20,4 +21,23 @@ func LoadJSON[T any](name, path string) (*T, error) {
 	}
 
 	return obj, nil
+}
+
+// Save the generic type to a JSON file.
+func SaveJSON[T any](name string, data *T, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return ChainErrorF(err, "error creating %s file", name)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	err = encoder.Encode(data)
+	if err != nil {
+		return ChainErrorF(err, "error encoding %s JSON", name)
+	}
+
+	return nil
 }
