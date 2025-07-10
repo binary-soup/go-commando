b562db0d804f2ffb996a087d96ff5b0cc69efa00
Ryan Stamp <apps@binarysoup.dev>
2025-07-09 22:00:52 -0400

    Add file system dependency docs

--- d/fs.go
+++ d/fs.go
@@ -2,6 +2,7 @@ package d
 
 import "github.com/binary-soup/go-command/d/fs"
 
+// Dependency object for a file system. Default implementation is Disk.
 var FileSystem = fileSystemDependency{
 	FileSystem: fs.Disk{},
 }
@@ -11,11 +12,16 @@ type fileSystemDependency struct {
 	cache fs.FileSystem
 }
 
+// Override the current implementation with the new one.
+// Primary used for testing and mocking.
+//
+// Note: Overriding is NOT thread-safe.
 func (d *fileSystemDependency) Override(new fs.FileSystem) {
 	d.cache = d.FileSystem
 	d.FileSystem = new
 }
 
+// Restore the dependency to its previous value.
 func (d *fileSystemDependency) Restore() {
 	d.FileSystem = d.cache
 }
