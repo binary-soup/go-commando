6662fe6594f7203867c690ffce8ce63698e6aa2d
Ryan Stamp <apps@binarysoup.dev>
2025-07-24 00:53:01 -0400

    Change config path for test and prod builds

--- util/data_path.go
+++ util/data_path.go
@@ -8,14 +8,20 @@ import (
 )
 
 // Get the path for storing user data.
-// Of the form <OS config dir>/<data dir>/<build mode>.
-//
 // Panics if the location cannot be determined.
+//
+// For Test builds, this is {OS config dir}/{data dir}_test.
+// For Production builds, this is just {OS config dir}/{data dir}.
 func UserDataPath(dataDir string) string {
 	path, err := os.UserConfigDir()
 	if err != nil {
 		panic(err)
 	}
+	path = filepath.Join(path, dataDir)
 
-	return filepath.Join(path, dataDir, build.GetType())
+	if build.IsProduction() {
+		return path
+	} else {
+		return path + "_test"
+	}
 }
